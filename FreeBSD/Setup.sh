e777c461d38457970752138124d556449b799753
Matthew Pherigo <hybrid120@gmail.com>
2014-04-10 22:42:07 -0500

    Setup.sh

---
+++ FreeBSD/Setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# If user "matt" exists and folder "dotfiles" does not exist in his home dir
+# 	-clone mwpher/dotfiles
+# 	-get sudo permissions
+# 		cp FreeBSD/...
+# 		    FreeBSD/etc.profile
+# 	            u.l.e.bash.bashrc
+# 		ln -s
+# 		    home.bashrc
+# 		    dotvim/
+# 	
+
+# If denyhosts is installed, then:
+# 	-Config it
+# 	-Setup hosts.allow to log to local7.notice
+# 	-Tell syslog to log to /var/log/wrapper.log
+
+
