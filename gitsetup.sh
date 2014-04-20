e52b95e3f906c76451d97002eb8e07d16b3344e6
Matthew Pherigo <hybrid120@gmail.com>
2014-04-20 19:28:33 -0400

    i dont like magic

--- gitsetup.sh
+++ gitsetup.sh
@@ -4,3 +4,4 @@ which git || return 1
 git config --global user.name "Matthew Pherigo"
 git config --global user.email hybrid120@gmail.com
 git commit --amend --reset-author
+git config --global push.default simple
