30d8d83c4588393a8fba35e4a63477966d43146d
Matthew Pherigo <hybrid120@gmail.com>
2014-11-28 13:49:38 -0600

    remove denyhosts

--- denyhosts/setup.sh
+++
@@ -1,2 +0,0 @@
-echo '/var/log/denyhosts 			644  7 	   1024 *     J     /var/run/denyhosts.pid' >> /etc/newsyslog.conf
-echo "# Log denyhosts messages\nlocal7.info /var/log/wrapper.log" >> /etc/syslog.conf
