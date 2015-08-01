56f59086c4691ce9787f5d901dc52d10c3d9ffa2
Matthew Pherigo <hybrid120@gmail.com>
2015-08-01 17:40:33 -0500

    fix excluded name

--- rsync.sh
+++ rsync.sh
@@ -1,4 +1,4 @@
 rsync -Pvrh --size-only --delete \
 	--exclude-from=rsync_exclude.txt \
-	/cygwin/e/ \
-	matt@192.168.1.29:/mnt/Data/E-Drive/
+	/cygdrive/e/ \
+	matt@192.168.1.29:/mnt/Data/Matt/E-Drive/
