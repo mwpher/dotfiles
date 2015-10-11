609141f0f37d329f945818cada65bbbc6b55c041
Matthew Pherigo <hybrid120@gmail.com>
2015-10-10 23:25:10 -0500

    keep retrying rsync

--- rsync.sh
+++ rsync.sh
@@ -1,4 +1,10 @@
-rsync -Pvrh --size-only --delete \
-	--exclude-from=rsync_exclude.txt \
-	/cygdrive/e/ \
-	Matthew@192.168.1.29:/mnt/Data/Matt/E-Drive/
+RC=1 
+while [[ RC -ne 0 ]]
+do
+
+	rsync -Pvrh --size-only --delete \
+		--exclude-from=rsync_exclude.txt \
+		/cygdrive/e/ \
+		Matthew@192.168.1.29:/mnt/Data/Matt/E-Drive/
+	RC=$?
+done
